b0661371f2de38b466f94b3c00823d183a2e8a47
naveen-iyer-13 <naveeniyer13@gmail.com>
2020-04-18 04:22:22 +0530

    Updates

---
+++ precache-manifest.d6009cf747c58977f5186d33b34e535f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca758b792cec2580354ea395fb52207",
+    "url": "/text-detection-tesseract/index.html"
+  },
+  {
+    "revision": "bfb84205ea6d697ecec9",
+    "url": "/text-detection-tesseract/static/css/main.79b476d2.chunk.css"
+  },
+  {
+    "revision": "b1c1a0d205f59d766fdd",
+    "url": "/text-detection-tesseract/static/js/2.a04175c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-detection-tesseract/static/js/2.a04175c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb84205ea6d697ecec9",
+    "url": "/text-detection-tesseract/static/js/main.71e15a0d.chunk.js"
+  },
+  {
+    "revision": "d1259b334704a8221138",
+    "url": "/text-detection-tesseract/static/js/runtime-main.96168cbb.js"
+  }
+]);
